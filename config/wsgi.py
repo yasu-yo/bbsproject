8d7114eeb9eb77380ffbdd9135701b1600ee0c83
fujiwara yasuyo <y.fujiwara.sys24@morijyobi.ac.jp>
2025-06-12 15:41:06 +0900

    fix wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,8 +8,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+import django # 追加
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
